c356adbaf07c17dfc2a9c47008bb401d6aa6d9f0
Samuel <95908184+platoni-c@users.noreply.github.com>
2025-11-18 15:21:52 +0300

    Fix image source path for hero banner

--- src/App.jsx
+++ src/App.jsx
@@ -81,7 +81,7 @@ const App = () => {
             <div className="pattern">
                 <div className="wrapper">
                     <header>
-                        <img src="../public/hero.png" alt="Hero Banner"/>
+                        <img src="../hero.png" alt="Hero Banner"/>
                         <h1>Find <span className={"text-gradient"}>Movies</span> You'll Enjoy Without Hassle</h1>
                         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
                     </header>
