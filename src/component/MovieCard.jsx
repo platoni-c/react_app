046d64017d3e902d197f1e8c7b9e2dc70f743d93
Samuel <95908184+platoni-c@users.noreply.github.com>
2025-11-18 15:23:01 +0300

    Update MovieCard.jsx

--- src/component/MovieCard.jsx
+++ src/component/MovieCard.jsx
@@ -4,7 +4,7 @@ const MovieCard = ({movie: {title, vote_average, poster_path, release_date, orig
     return (
         <div className={"movie-card"}>
             <img src={poster_path ?
-                `https://image.tmdb.org/t/p/w500/${poster_path}` : "../../public/no-movie.png"} alt={title}/>
+                `https://image.tmdb.org/t/p/w500/${poster_path}` : "../../no-movie.png"} alt={title}/>
             <div className="mt-4">
                 <h3>{title}</h3>
                 <div className="content">
